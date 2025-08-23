c9f7e5d09e2d827ae31190767de31ce2a8bccaeb
Tomsooer <tsoer2013@gmail.com>
2025-08-23 14:24:50 +0300

    Always and People section

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-document.getElementById('openVideo').addEventListener('click', function (e) {
-	e.preventDefault();
+document.getElementById('openVideo').addEventListener('click', function () {
 	const modal = document.getElementById('videoModal');
-	const iframe = document.getElementById('youtubeVideo');
-	iframe.src = 'https://www.youtube.com/embed/d5K-A3Lp9ns?si=m2SqHk7hRj5w58mF';
-	// "https://www.youtube.com/embed/d5K-A3Lp9ns?si=m2SqHk7hRj5w58mF"
-	// iframe.src = 'https://www.youtube.com/watch?v=d5K-A3Lp9ns';
-	// src="https://www.youtube.com/embed/CQzZacgAmek?si=Ie1bIwurnfqtroud"
 	modal.style.display = 'flex';
 });
 
-document.querySelector('.close').addEventListener('click', function () {
+document.getElementById('closeVideo').addEventListener('click', function () {
 	const modal = document.getElementById('videoModal');
-	const iframe = document.getElementById('youtubeVideo');
-	iframe.src = '';
 	modal.style.display = 'none';
 });
 
